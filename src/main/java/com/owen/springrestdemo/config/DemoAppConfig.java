7bf5e9353f12ec21bc282bfdf434e1452126011d
yuchlin <yuchlin@ebay.com>
2019-04-09 00:02:51 -0700

    Rest hello world

---
+++ src/main/java/com/owen/springrestdemo/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.owen.springrestdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.owen.springrestdemo")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+}
