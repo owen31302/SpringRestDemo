5d6ce1672a7b81e0e2491f706e522e1fa5796ef5
yuchlin <yuchlin@ebay.com>
2019-04-19 22:33:40 -0700

    Exception Handling - Adding Custom Exception Handler

--- src/main/java/com/owen/springrestdemo/controller/StudentRestController.java
+++ src/main/java/com/owen/springrestdemo/controller/StudentRestController.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 
 import com.owen.springrestdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 
@@ -38,7 +37,27 @@ public class StudentRestController {
 	// define endpoint for "/students/{studentId}" - return student at index
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId){
+
+		// check the studentId against list size
+
+		if ( (studentId >= theStudents.size()) || (studentId < 0) ) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+
 		return theStudents.get(studentId);
+
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+		StudentErrorResponse error = new StudentErrorResponse();
+
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 	
 }
